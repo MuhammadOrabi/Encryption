07bc75154414c2f80fccbc9e33cd610893a6b445
MuhammadOrabi <muhammad.orabi,2014@hotmail.com>
2016-06-12 12:46:05 +0200

    some_message

---
+++ Encryption/Encryption/Ceaser.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    class Ceaser
+    {
+        string alpha;
+        string Alpha;
+        string encrypted;
+        string decrypted;
+        public Ceaser()
+        {
+            alpha = "abcdefghijklmnopqrstuvwxyz";
+            Alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        }
+        public string enc (string text, int key)
+        {
+            encrypted = "";
+            for (int i = 0 ; i < text.Length ; i++)
+            {
+                if(char.IsLower(text[i]))
+                {
+                    int tmp = 0;
+                    for (int j = 0; j < alpha.Length; j++)
+                    {
+                        if (alpha[j] == text[i])
+                        {
+                            tmp = j + key;
+                            while(tmp >= alpha.Length)
+                            {
+                                tmp = tmp - alpha.Length;
+                            }
+                            encrypted += alpha[tmp];
+                            break;
+                            
+                        }
+                    }
+                }
+                else if(char.IsUpper(text[i]))
+                {
+                    int tmp = 0;
+                    for (int j = 0; j < Alpha.Length; j++)
+                    {
+                        if (Alpha[j] == text[i])
+                        {
+                            tmp = j + key;
+                            while (tmp >= Alpha.Length)
+                            {
+                                tmp = tmp - Alpha.Length;
+                            }
+                            encrypted += Alpha[tmp];
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    encrypted += text[i];
+                }
+            }
+            return encrypted;
+        }
+        public string dec(string text, int key)
+        {
+            decrypted = "";
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsLower(text[i]))
+                {
+                    int tmp = 0;
+                    for (int j = 1; j < alpha.Length; j++)
+                    {
+                        if (alpha[j] == text[i])
+                        {
+                            tmp = j - key;
+                            while (tmp < 0)
+                            {
+                                tmp = alpha.Length + tmp;
+                            }
+                            decrypted += alpha[tmp];
+                            break;
+                        }
+                    }
+                }
+                else if (char.IsUpper(text[i]))
+                {
+                    int tmp = 0;
+                    for (int j = 1; j < Alpha.Length; j++)
+                    {
+                        if (Alpha[j] == text[i])
+                        {
+                            tmp = j - key;
+                            while (tmp < 0)
+                            {
+                                tmp = Alpha.Length + tmp;
+                            }
+                            decrypted += Alpha[tmp];
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    decrypted += text[i];
+                }
+            }
+            return decrypted;
+        } 
+    }
+}
---
+++ Encryption/Encryption/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Encryption
+{
+    public partial class Form1 : Form
+    {
+        string nfile_name = "";
+        string efile_name = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            try {
+                if (methods.Text == "Ceaser Cipher")
+                {
+                    Ceaser c = new Ceaser();
+                    if (nfile.Text != "")
+                    {
+                        efile.Text = c.enc(nfile.Text, int.Parse(key.Text));
+                    }
+                    else if (nfile_name != "" && efile_name != "")
+                    {
+                        string line = "";
+                        FileStream fs = new FileStream(nfile_name, FileMode.Open, FileAccess.Read);
+                        FileStream fs2 = new FileStream(efile_name, FileMode.OpenOrCreate, FileAccess.Write);
+                        StreamReader sr = new StreamReader(fs);
+                        StreamWriter sw = new StreamWriter(fs2);
+                        while((line = sr.ReadLine())!=null)
+                        {
+                            string[] tmp = line.Split(' ');
+                            foreach(string i in tmp)
+                            {
+                                sw.Write(c.enc(i,int.Parse(key.Text)) + " ");
+                            }
+                            sw.Write("\r\n");
+                        }
+                        sw.Flush();
+                        fs.Close();
+                        fs2.Close();
+                        MessageBox.Show("Done");
+                    }
+                }
+
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+         }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try {
+                if (methods.Text == "Ceaser Cipher")
+                {
+                    Ceaser c = new Ceaser();
+                    if (efile.Text != "")
+                    {
+                        nfile.Text = c.dec(efile.Text, int.Parse(key.Text));
+                    }
+                    else if (nfile_name != "" && efile_name != "")
+                    {
+                        string line = "";
+                        FileStream fs = new FileStream(nfile_name, FileMode.Open, FileAccess.Write);
+                        FileStream fs2 = new FileStream(efile_name, FileMode.OpenOrCreate, FileAccess.Read);
+                        StreamReader sr = new StreamReader(fs2);
+                        StreamWriter sw = new StreamWriter(fs);
+                        while ((line = sr.ReadLine()) != null)
+                        {
+                            string[] tmp = line.Split(' ');
+                            foreach (string i in tmp)
+                            {
+                                sw.Write(c.dec(i, int.Parse(key.Text)) + " ");
+                            }
+                            sw.Write("\r\n");
+                        }
+                        sw.Flush();
+                        sw.Flush();
+                        fs.Close();
+                        fs2.Close();
+                        MessageBox.Show("Done");
+                    }
+                }
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fc = new OpenFileDialog();
+            DialogResult res = fc.ShowDialog();
+            if (res == DialogResult.Cancel)
+                return;
+            nfile_name = fc.FileName;
+        }
+        
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fc = new OpenFileDialog();
+            DialogResult res = fc.ShowDialog();
+            if (res == DialogResult.Cancel)
+                return;
+            efile_name = fc.FileName;
+        }
+    }
+}
